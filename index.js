b96663bc85809c90b1db3e7140d398189ff988a6
Seth Blanchard <nsethblanchard@gmail.com>
2021-04-22 13:44:01 -0400

    commit tree 9b5b36d12803e48e125ef2c753b5269b682309aa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+function superbowlWin(array){
+    // for (const year of array) {
+    //     if (year.result === "W") {
+    //         return year.year
+    //     }
+    // }
+
+    let asdf = array.find(year => year.result === "W")
+    console.log(asdf)
+    if (asdf !== undefined) {
+        return asdf.year
+    }
+    // array.find(function(year) {
+    //     if (year.result === "W") {
+    //         console.log(year)
+    //     }
+    // })
+}
